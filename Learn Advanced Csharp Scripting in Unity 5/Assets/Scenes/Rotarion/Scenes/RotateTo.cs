fc67d815e976985fb83420456419016a9ca3fa51
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-08 00:26:42 -0300

    10-Rotation and Looking

--- Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/Rotarion/Scenes/RotateTo.cs
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/Rotarion/Scenes/RotateTo.cs
@@ -8,6 +8,8 @@ public class RotateTo : MonoBehaviour
     private Transform thisTransform;
     public float rotSpeed = 90f;
 
+    public Transform target = null;
+
     private void Awake() {
         
         thisTransform = this.gameObject.GetComponent<Transform>();
@@ -23,8 +25,6 @@ public class RotateTo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //thisTransform.rotation = Quaternion.Euler(0f, 90f, 0f);
-        
-        thisTransform.rotation *= Quaternion.AngleAxis(rotSpeed * Time.deltaTime, Vector3.up);
+        thisTransform.rotation = Quaternion.LookRotation(target.position - thisTransform.position, Vector3.up);
     }
 }
