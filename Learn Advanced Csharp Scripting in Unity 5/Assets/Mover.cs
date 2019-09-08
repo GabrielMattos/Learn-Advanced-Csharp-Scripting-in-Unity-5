78ffe9aed900dcb77b488e176584343617f937e7
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-07 23:28:46 -0300

    04-Vectors and Coordinate Spaces

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Mover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    public float maxSpeed = 1f;
+    private Transform thisTranform = null;
+
+    void Awake() {
+
+        thisTranform = this.gameObject.GetComponent<Transform>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisTranform.position += thisTranform.forward * maxSpeed;
+    }
+}
