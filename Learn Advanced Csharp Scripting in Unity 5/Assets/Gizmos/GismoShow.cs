9e7117455f605ac8099fafd5d65d3db5a132f70e
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-07 23:51:46 -0300

    06-Gizmos

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Gizmos/GismoShow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GismoShow : MonoBehaviour
+{
+
+    public bool showGizmos = true;
+
+    public string myIcon = string.Empty;
+
+    [Range(0f, 100f)]
+    public float range = 10f;
+
+    void OnDrawGizmos() {
+        
+        if(!showGizmos) {
+            return;
+        }
+
+        Gizmos.DrawIcon(transform.position, myIcon, true);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, range);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * range);
+
+
+    }
+}
