8a76e9158349c5aa6e42a97aa3e59bcb1cf2e74a
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-08 00:19:30 -0300

    09-Rotation and Circular Motion

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/Rotarion/Scenes/RotateTo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTo : MonoBehaviour
+{
+
+    private Transform thisTransform;
+    public float rotSpeed = 90f;
+
+    private void Awake() {
+        
+        thisTransform = this.gameObject.GetComponent<Transform>();
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //thisTransform.rotation = Quaternion.Euler(0f, 90f, 0f);
+        
+        thisTransform.rotation *= Quaternion.AngleAxis(rotSpeed * Time.deltaTime, Vector3.up);
+    }
+}
