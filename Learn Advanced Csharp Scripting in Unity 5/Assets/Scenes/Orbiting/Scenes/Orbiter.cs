5633a25192d612307d1f8e69d7a7361882f77152
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-08 00:51:02 -0300

    12-Orbiting and Movement with Quaternions

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/Orbiting/Scenes/Orbiter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Orbiter : MonoBehaviour
+{
+
+    public Transform pivot = null;
+    private Transform thisTransform = null;
+    private Quaternion destRot = Quaternion.identity;
+    public float pivotDistance = 5f;
+    public float rotSpeed = 10f;
+    private float rotX = 0f;
+    private float rotY = 0f;
+
+    private void Awake() {
+        thisTransform = this.gameObject.GetComponent<Transform>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horz = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vert = CrossPlatformInputManager.GetAxis("Vertical");
+
+        rotX += vert * Time.deltaTime * rotSpeed;
+        rotY += horz * Time.deltaTime * rotSpeed;
+
+        Quaternion yRot = Quaternion.Euler(0f, rotY, 0f);
+        destRot = yRot * Quaternion.Euler(rotX, 0f, 0f);
+
+        thisTransform.rotation = destRot;
+
+        //Adjust position
+        thisTransform.position = pivot.position + thisTransform.rotation * Vector3.forward * -pivotDistance;
+    }
+}
