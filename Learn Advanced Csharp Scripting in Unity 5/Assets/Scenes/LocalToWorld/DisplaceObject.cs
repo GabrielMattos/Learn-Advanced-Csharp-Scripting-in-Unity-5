1b1d1cfd16d80b6788812c2fcc5a3c239ac259f9
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-09 13:25:32 -0300

    04-World Space vs Local Space

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/LocalToWorld/DisplaceObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplaceObject : MonoBehaviour
+{
+
+    public float displaceSpeed = 2f;
+    private Transform thisTransform = null;
+
+    [SerializeField]
+    private Vector3 localForward;
+    [SerializeField]
+    private Vector3 transformForward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTransform = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        localForward = Vector3.forward;
+        transformForward = thisTransform.forward;
+
+        thisTransform.position += thisTransform.forward * displaceSpeed * Time.deltaTime;
+    }
+}
