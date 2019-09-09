b9c3f712d848bf31a3ee4cc43f4b5cb92d449cb7
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-09 10:35:51 -0300

    Section 2 Gravity, Jumping, and basic Physics - 02-Player Movement

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/Mover/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float maxSpeed = 10f;
+    public float rotSpeed = 5f;
+    private Transform thisTranform = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTranform = this.gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horz = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vert = CrossPlatformInputManager.GetAxis("Vertical");
+
+        thisTranform.rotation *= Quaternion.Euler(0f, rotSpeed * horz * Time.deltaTime, 0f);
+
+        thisTranform.position += thisTranform.forward * maxSpeed * vert * Time.deltaTime;
+    }
+}
