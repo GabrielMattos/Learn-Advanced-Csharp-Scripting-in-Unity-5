a15940c9cb4f118b78861f2ce576997b43fd1fe9
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-09 11:06:50 -0300

    03-Physics and Collisions

--- Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/Mover/PlayerController.cs
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/Mover/PlayerController.cs
@@ -9,21 +9,24 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed = 10f;
     public float rotSpeed = 5f;
     private Transform thisTranform = null;
+    private CharacterController thisController = null;
 
     // Start is called before the first frame update
     void Start()
     {
         thisTranform = this.gameObject.GetComponent<Transform>();
+        thisController = this.gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float horz = CrossPlatformInputManager.GetAxis("Horizontal");
         float vert = CrossPlatformInputManager.GetAxis("Vertical");
 
         thisTranform.rotation *= Quaternion.Euler(0f, rotSpeed * horz * Time.deltaTime, 0f);
 
-        thisTranform.position += thisTranform.forward * maxSpeed * vert * Time.deltaTime;
+        //thisTranform.position += thisTranform.forward * maxSpeed * vert * Time.deltaTime;
+        thisController.SimpleMove(thisTranform.forward * maxSpeed * vert);
     }
 }
