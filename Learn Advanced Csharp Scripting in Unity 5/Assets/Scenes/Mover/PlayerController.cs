2c6cb7afe4c503443e892c98cb23828ab71ed0c2
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-09 14:44:59 -0300

    06-Gravity and Jumping06-Gravity and Jumping

--- Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/Mover/PlayerController.cs
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/Mover/PlayerController.cs
@@ -7,14 +7,19 @@ public class PlayerController : MonoBehaviour
 {
 
     public float maxSpeed = 10f;
-    public float rotSpeed = 5f;
-    private Transform thisTranform = null;
+    public float rotateSpeed = 5f;
+    private Transform thisTransform = null;
     private CharacterController thisController = null;
+    public float jumpForce = 50f;
+    public float groundDist = 0.1f;
+    public bool isGrounded = false;
+    private Vector3 velocity = Vector3.zero;
+    public LayerMask groundLayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        thisTranform = this.gameObject.GetComponent<Transform>();
+        thisTransform = this.gameObject.GetComponent<Transform>();
         thisController = this.gameObject.GetComponent<CharacterController>();
     }
 
@@ -24,9 +29,33 @@ public class PlayerController : MonoBehaviour
         float horz = CrossPlatformInputManager.GetAxis("Horizontal");
         float vert = CrossPlatformInputManager.GetAxis("Vertical");
 
-        thisTranform.rotation *= Quaternion.Euler(0f, rotSpeed * horz * Time.deltaTime, 0f);
+        thisTransform.rotation *= Quaternion.Euler(new Vector3(0f, rotateSpeed * horz * Time.deltaTime, 0f));
+
+        //CALCULATE MOVE DIR
+        velocity.z = vert * maxSpeed;
+
+        //ARE WE GROUNDED?
+        isGrounded = (DistanceToGround() < groundDist) ? true : false;
+
+        //SHOULD WE JUMP?
+        if(CrossPlatformInputManager.GetAxisRaw("Jump")!=0 && isGrounded)
+            velocity.y = jumpForce;
+
+        //APPLY GRAVITY
+        velocity.y += Physics.gravity.y * Time.deltaTime;
 
         //thisTranform.position += thisTranform.forward * maxSpeed * vert * Time.deltaTime;
-        thisController.SimpleMove(thisTranform.forward * maxSpeed * vert);
+        //thisController.SimpleMove(thisTransform.forward * maxSpeed * vert);
+        //MOVE
+        thisController.Move(thisTransform.TransformDirection(velocity) * Time.deltaTime);
+    }
+
+    public float DistanceToGround() {
+
+        RaycastHit hit;
+        float distanceToGround = 0f;
+        if(Physics.Raycast(thisTransform.position, -Vector3.up, out hit, Mathf.Infinity, groundLayer))
+            distanceToGround = hit.distance;
+        return distanceToGround;
     }
 }
