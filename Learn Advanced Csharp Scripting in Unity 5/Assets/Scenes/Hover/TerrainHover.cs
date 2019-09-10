b54cece5d63d80f043dfaf095e94e11e710bb311
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-10 11:40:03 -0300

    10-Terrain Planar Movement

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/Hover/TerrainHover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class TerrainHover : MonoBehaviour
+{
+
+    private Transform thisTransform = null;
+    public float maxSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horz = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vert = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 newPos = thisTransform.position;
+        newPos += thisTransform.forward * vert * maxSpeed * Time.deltaTime;
+        newPos += thisTransform.right * horz * maxSpeed * Time.deltaTime;
+
+        thisTransform.position = newPos;
+    }
+}
