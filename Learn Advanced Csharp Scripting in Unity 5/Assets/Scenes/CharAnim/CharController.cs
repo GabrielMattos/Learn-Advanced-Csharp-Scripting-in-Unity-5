0ce90b99604431cf2ab3cf33fd7c8ecca9ffb402
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-11 13:37:16 -0300

    06-Scripting with Blend Trees

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/CharAnim/CharController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharController : MonoBehaviour
+{
+
+    private Animator thisAnimator = null;
+    private int vertHash = Animator.StringToHash("Vertical");
+    private int horzHash = Animator.StringToHash("Horizontal");
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horz = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vert = CrossPlatformInputManager.GetAxis("Vertical");
+
+        thisAnimator.SetFloat(horzHash, horz, 0.1f, Time.deltaTime);
+        thisAnimator.SetFloat(vertHash, vert, 0.1f, Time.deltaTime);
+    }
+}
