5b6111b36b94b5e49d3fdba5979b47e338a9406d
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-11 17:16:57 -0300

    12-Scripting Inventory Items

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/CharAnim/InventoryItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : MonoBehaviour
+{
+
+    public enum ITEMTYPE {SPHERE, BOX, CYLINDER};
+    public ITEMTYPE type;
+    public Sprite GUI_Icon = null;
+
+    void OnTriggerEnter(Collider target) {
+
+        if(!target.CompareTag("Player")) {
+            return;
+        }
+
+        
+    }
+}
