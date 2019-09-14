1872239b12ed5af5d05fcea0f6cb3a0b4ad25491
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-14 18:00:31 -0300

    10-Creating an Ammo Manager

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/AmmoCreation/AmmoManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoManager : MonoBehaviour
+{   
+    public GameObject ammoPrefab = null;
+
+    public int poolSize = 100;
+
+    private GameObject[] ammoArray;
+
+    public static AmmoManager ammoManagerSingleton = null;
+
+    private void Awake() {
+        
+        if(ammoManagerSingleton != null) {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        ammoManagerSingleton = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
