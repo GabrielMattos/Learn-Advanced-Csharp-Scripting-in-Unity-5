1048a706d2ff36992d7a237926893ddb0d31dc3b
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-14 17:55:53 -0300

    09-Scripting Ammo

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/AmmoCreation/Ammo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+
+    public float lifeTime = 2f;
+
+    private void OnEnable() {
+        
+        Invoke("Die", lifeTime);
+    }
+
+    void Die() {
+
+        CancelInvoke();
+        gameObject.SetActive(false);
+    }
+}
