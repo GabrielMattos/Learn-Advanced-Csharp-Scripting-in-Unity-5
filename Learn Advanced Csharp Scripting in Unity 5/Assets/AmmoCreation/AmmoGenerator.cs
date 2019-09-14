7888b5472704a491656a8172042453e93431550c
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-14 17:48:05 -0300

    07-Instantiating Ammo

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/AmmoCreation/AmmoGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoGenerator : MonoBehaviour
+{
+
+    public GameObject ammoPrefab = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) {
+            Instantiate(ammoPrefab, transform.position, transform.rotation);
+        }
+    }
+}
