4e7fa230115d4265aa9a2f600903cad9776c1a2e
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-14 19:50:14 -0300

    04-Field of View

--- Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/LineOfSight/LineSight.cs
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/LineOfSight/LineSight.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class LineSight : MonoBehaviour
 {
+
+    public float fieldOfView = 45f;
+
+    public Transform target = null;
+    public Transform eyePoint = null;
+
+    public bool canSeeTarget = false;
+
     private void OnTriggerStay(Collider target) {
+
+        canSeeTarget = InFOV();
+    }
+
+    bool InFOV() {
+
+        //Get direction to target
+        Vector3 dirToTarget = target.position - eyePoint.position;
+
+        //get angles between forward and look direction
+        float angle = Vector3.Angle(eyePoint.forward, dirToTarget);
+
+        //are we within field of view?
+        if(angle <= fieldOfView)
+            return true;
         
-        print(target.name);
+        //not within field
+        return false;
     }
 }
