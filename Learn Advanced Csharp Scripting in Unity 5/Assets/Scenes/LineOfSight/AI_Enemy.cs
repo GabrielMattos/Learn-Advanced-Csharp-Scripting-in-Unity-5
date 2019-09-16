1e7200906ade33ebef31fd4da645e4d56c949df0
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-15 23:51:05 -0300

    07-Path-Finding and Steering

---
+++ Learn Advanced Csharp Scripting in Unity 5/Assets/Scenes/LineOfSight/AI_Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Enemy : MonoBehaviour
+{
+
+    private UnityEngine.AI.NavMeshAgent thisAgent = null;
+
+    public Transform patrolDestination = null;
+
+    private void Awake() {
+        
+        thisAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisAgent.SetDestination(patrolDestination.position);
+    }
+}
